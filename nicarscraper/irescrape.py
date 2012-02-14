668ea0d6ca7295a6a7d3a536e85e90276d3b5af2
Tom Meagher <car+bit@tommeagher.com>
2012-02-14 16:07:02 -0500

    added speakers

--- nicarscraper/irescrape.py
+++ nicarscraper/irescrape.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 from mechanize import Browser
 from BeautifulSoup import BeautifulSoup
+#import codecs
+#import sys
 
-#outfile = open("nicarscraped.txt", "w")
+#streamWriter = codecs.lookup('utf-8')[-1]
+#sys.stdout = streamWriter(sys.stdout)
+
+outfile = open("nicarscraped.txt", "w")
 
 mech = Browser()
 url = "http://ire.org/conferences/nicar-2012/schedule/"
@@ -13,10 +18,15 @@ soup = BeautifulSoup(html)
 
 for row in soup.findAll('h3', {"class" : "title3"}):
     name = row.find('a').string
-    desc = row.findNext('div', {"class" : "col-15 meta"}).p.string
-    time = desc.findNext('p').string
-    record = (name, desc, time)
-    print "; ".join(record)
+    speaker = name.findNext('p')
+    subtree = speaker.strong
+    subtree.extract()
+    speaker2 = speaker.string
+    place = row.findNext('div', {"class" : "col-15 meta"}).p.string
+    time = place.findNext('p').string
+    record = (name, speaker2, place, time)
+#    print record
 #    print >> outfile, "; ".join(record)
+    print "; ".join(record)
 
-#outfile.close()
+outfile.close()
