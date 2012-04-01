59331e17800fc83783c5a02bbca406b58539a4e3
Tom Meagher <car+bit@tommeagher.com>
2012-03-31 21:58:02 -0400

    fixed scraper and CSV

--- townsites/townscrape.py
+++ townsites/townscrape.py
@@ -1,9 +1,12 @@
 #1/usr/bin/env python
 from mechanize import Browser
 from BeautifulSoup import BeautifulSoup
+from csvkit.unicsv import UnicodeCSVWriter
 import re
 
-outfile = open("townsites.csv", "w")
+outfile = open("sitesdirt.csv", "w")
+w = UnicodeCSVWriter(outfile,delimiter=",",encoding="Cp1252")
+w.writerow(['name','url'])
 
 mech = Browser()
 url = "http://www.state.nj.us/nj/govinfo/county/localgov.html"
@@ -13,26 +16,25 @@ html = page.read()
 soup = BeautifulSoup(html)
 
 for row in soup.findAll('div', {"id" : "anchorSection"}):
-    for row in row.findAll('ul'):
-        for row in row.findAll('li'):
-            for anchor in row.findAll('a', href=True):
-                name = anchor.string
-                for row in anchor:
-                    url = anchor['href'].decode() 
-                print >> outfile, name, ",", url
+#    for row in row.findAll('ul'):
+#    for row in row.findAll('li'):
+    for anchor in row.findAll('a', href=True):
+        name = anchor.string
+        url = anchor['href'].decode() 
+        record = (name, url)
+        w.writerow(record)
 
 outfile.close()
 
 #now add a re parser to clean it up for import
-#outfile2 = open("townsites.csv", "r")
+infile = open("sitesdirt.csv", "r")
+outfile = open("townsites.csv", "w")
 
-        #for link in row.findAll('a'):
-           # print link['href']
-           # print url.string
-#    street = row.find('div', {"class" : "GuideResultAddress"}).span.string
-#    city = street.findNext('span').string
-#    record = (name, street, city)
-#    print >> outfile, ", ".join(record)
+for line in infile:
+    if re.match("name,url", line):
+        print >> outfile,line,    
+    if re.findall(".http:", line):
+        print >> outfile,line,
 
-#outfile.close()
-
+infile.close()
+outfile.close()
